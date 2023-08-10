7a6b1bb78ba4609d7b1482a3672f9acf47b06529
MacaCoronel93 <macarenajecoronel@gmail.com>
2023-08-09 23:02:28 -0300

    agregamos helpers y chequeos de usuarios

--- routes/usuario.js
+++ routes/usuario.js
@@ -1,10 +1,22 @@
 const { Router } = require('express');
-const {usuariosGet,usuariosPost,usuariosPut,usuariosDelete}= require ('../controllers/usuario')
+const {check} = require('express-validator')
+const {usuariosGet,usuariosPost,usuariosPut,usuariosDelete}= require ('../controllers/usuario');
+const { emaiExiste, esRolValido } = require('../helpers/db validator');
+const { validarCampos } = require('../middlewares/validar_campos');
 const router = Router();
 
 router.get ('/', usuariosGet);
-router.post ('/', usuariosPost);
+
+router.post ('/', [check ('nickname', 'El nombre es requerido').notEmpty(),
+                    check('password', 'La contraseña tiene que tener minimo 8 caracteres').isLength({min:6}),
+                    check('mail').custom(emaiExiste),
+                    check('rol').custom(esRolValido),
+                    validarCampos
+                    ],
+usuariosPost);
+
 router.put ('/:id', usuariosPut);
+
 router.delete ('/:id', usuariosDelete);
 
 
