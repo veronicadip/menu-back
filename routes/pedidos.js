786a9574c665e32b7faba861f623d0ea4428503c
MacaCoronel93 <macarenajecoronel@gmail.com>
2023-08-17 01:25:50 -0300

    subir pedidos , controlers y rutas

--- routes/pedidos.js
+++ routes/pedidos.js
@@ -1,7 +1,23 @@
 const { Router } = require('express');
 const {check} = require('express-validator');
-const{pedidosGet} = require ('../controllers/pedidos')
+const{pedidosGet,pedidosPost,pedidosPut,pedidosDelete} = require ('../controllers/pedidos')
 const { emailExiste, esRolValido, usuarioExiste } = require('../helpers/db validator');
 const { validarCampos } = require('../middlewares/validar_campos');
 const { validarJWT } = require('../middlewares/validar_jwt');
 const { esAdminRole } = require('../middlewares/validar_roles');
+
+const router = Router();
+
+router.get ('/',[validarJWT,
+                check("id").custom(usuarioExiste),
+], pedidosGet);
+
+
+router.post ('/', pedidosPost);
+
+
+
+router.put ('/', pedidosPut);
+
+
+router.delete ('/', pedidosDelete);
